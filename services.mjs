d7b933b9e6e4bcc141ac260a187305a912ffe8cf
Anders Houbak Kristiansen <palantus@gmail.com>
2022-11-25 13:26:45 +0100

    Added data upgrade to fix missing data

--- services.mjs
+++ services.mjs
@@ -1,3 +1,11 @@
-export default async () => {return {
+import Entity, { query, runJob } from "entitystorage"
+
+export default async () => {
+
+  if(!query.tag("fix_pre_2_0_14_rels").first){
+    runJob("fix_pre_2_0_14_rels")
+    new Entity().tag("fix_pre_2_0_14_rels")
+  }
   
-}}
+  return {}
+}
