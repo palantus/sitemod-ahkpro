210a37e65dc91322dc59406f0c8ff08066f886f3
Anders Houbak Kristiansen <palantus@gmail.com>
2023-02-13 13:14:12 +0100

    Remove obsolete jobs

--- services.mjs
+++ services.mjs
@@ -2,10 +2,5 @@ import Entity, { query, runJob } from "entitystorage"
 
 export default async () => {
 
-  if(!query.tag("fix_pre_2_0_14_rels").first){
-    runJob("fix_pre_2_0_14_rels")
-    new Entity().tag("fix_pre_2_0_14_rels")
-  }
-  
   return {}
 }
